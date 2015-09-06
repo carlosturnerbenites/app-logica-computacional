35d6ac3ede480d8cefe44272b0c2419135daf770
Carlos Alonso Turner Benites <carlosturnerbenites@gmial.com>
2015-09-06 16:41:41 -0500

    creacion de tablas dinamica, creacion de funcion

--- logica computacional.js
+++ logica computacional.js
@@ -1,20 +1,45 @@
-var tablaY = [["p","q","p y q"],["V","V","F"],["V","F","V"],["V","F","F"],["F","F","F"]]
+var tablaY = 	[["p","q","p y q"],
+["V","V","F"],
+["V","F","V"],
+["V","F","F"],
+["F","F","F"]]
+var tablaO = 	[["p","q","p o q"],
+["V","V","F"],
+["V","F","F"],
+["V","F","F"],
+["F","F","V"]]
+var tablaC = 	[["p","q","p -> q"],
+["V","V","F"],
+["V","F","F"],
+["V","F","V"],
+["F","F","F"]]
+var tablaBC = 	[["p","q","p <-> q"],
+["V","V","V"],
+["V","F","F"],
+["V","F","F"],
+["F","F","V"]]
 
-var tableHTML = document.createElement("table")
+function crearTabla(tabla) {
+	var tableHTML = document.createElement("table")
 
-for (var filas = 0; filas < tablaY.length; filas++) {
+	for (var filas = 0; filas < tabla.length; filas++) {
 
-	var trHTML =document.createElement("tr")
+		var trHTML =document.createElement("tr")
 
-	for (var columnas = 0; columnas < tablaY[filas].length; columnas++) {
+		for (var columnas = 0; columnas < tabla[filas].length; columnas++) {
 
-		var thHTML =document.createElement("th")
-		thHTML.innerHTML = tablaY[filas][columnas]
-		trHTML.appendChild(thHTML)
+			var thHTML =document.createElement("th")
+			thHTML.innerHTML = tabla[filas][columnas]
+			trHTML.appendChild(thHTML)
+
+		};
+
+		tableHTML.appendChild(trHTML)
 
 	};
+	document.body.appendChild(tableHTML)
+}
 
-	tableHTML.appendChild(trHTML)
+crearTabla(tablaC)
 
-};
-document.body.appendChild(tableHTML)
+
