e3492440d0d19cbc85b1482333d2df034aae12c6
Carlos Alonso Turner Benites <carlosturnerbenites@gmial.com>
2015-09-11 13:20:36 -0500

    creacion de modulo de conjuntos

--- js/ejerciciosConjuntos.js
+++ js/ejerciciosConjuntos.js
@@ -2,6 +2,8 @@
 var formConjuntos = document.getElementById("formConjuntos")
 
 
+var contenedorConjuntoEscogido = document.getElementById("contenedorConjuntoEscogido")
+
 var seccionRespuesta = document.getElementById("seccionRespuesta")
 formConjuntos.addEventListener("submit", capturarConjunto)
 
@@ -10,14 +12,22 @@ function capturarConjunto(evento) {
 	evento.preventDefault()
 
 	var nombreConjunto = document.getElementById("nombreConjunto").value
-	var elementosCojuntos = (document.getElementById("elementosCojuntos").value).split(" ")
+
+	contenedorNombreConjunto.innerHTML = nombreConjunto
+
+	var elementosCojuntos = (document.getElementById("elementosCojuntos").value).split(",")
+	contenedorConjuntoEscogido.innerHTML = elementosCojuntos
 	console.log(elementosCojuntos);
 
 	var respuesta = document.createElement("textarea")
+	var buttonValidar = document.createElement("button")
+
+	buttonValidar.innerHTML = "Validar"
 	respuesta.id = "respuesta"
 	respuesta.classList.add("respuesta")
 	respuesta.setAttribute("placeholder", "Escriba en este cuadro los posibles subconjuntos del conjunto anterior descrito.")
 	console.log(respuesta);
 	seccionRespuesta.appendChild(respuesta)
+	seccionRespuesta.appendChild(buttonValidar)
 
 }
